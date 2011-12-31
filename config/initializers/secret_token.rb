6b54804f10daef51b595bfbccc6450367fd0834a
Jordan Quint <jordanquint@hotmail.com>
2011-12-30 22:47:27 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoralStory::Application.config.secret_token = '652f74bcf0f8494b6d4f334755c871ec5a5d72963361bcd9707b38f8da96b00d4bd0002085a4f3ef31c176bc7f9d3899dcc59c478a1997eed756efd37e2ff099'
